1b7d4d65fbfdc193b67fd95ab5b27e3a9050ebf6
Andrii Romaniuk <andrewrom321@gmail.com>
2024-09-11 15:24:30 +0300

    first commit

---
+++ src/test/java/ua/edu/ukma/event_management_system/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.event_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
