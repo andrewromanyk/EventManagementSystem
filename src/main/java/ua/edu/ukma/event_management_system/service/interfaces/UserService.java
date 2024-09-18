2b0c562c0d59a9357421f3f903eeeb26826c862e
Anton Ruban <antonruban03012005@gmail.com>
2024-09-18 15:59:02 +0300

    some interfaces

--- src/main/java/ua/edu/ukma/event_management_system/service/interfaces/UserService.java
+++ src/main/java/ua/edu/ukma/event_management_system/service/interfaces/UserService.java
@@ -1,4 +1,15 @@
 package ua.edu.ukma.event_management_system.service.interfaces;
 
+import ua.edu.ukma.event_management_system.entity.User;
+
 public interface UserService {
+
+    void registerUser(User user);
+
+    User getUserById(int userId);
+
+    void updateUser(User updatedUser);
+
+    void removeUser(int userId);
+
 }
