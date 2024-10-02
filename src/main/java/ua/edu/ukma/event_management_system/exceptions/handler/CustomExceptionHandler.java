bc330f7e32c0528d2dcb6ae76754b9742bcb8477
Andrii Romaniuk <andrewrom321@gmail.com>
2024-10-02 20:11:22 +0300

    ControllerAdvice added (it is automatically managed by spring)

---
+++ src/main/java/ua/edu/ukma/event_management_system/exceptions/handler/CustomExceptionHandler.java
@@ -0,0 +1,19 @@
+package ua.edu.ukma.event_management_system.exceptions.handler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
