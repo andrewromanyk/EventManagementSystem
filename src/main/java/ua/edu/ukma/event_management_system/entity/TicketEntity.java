29c7ad06b0c5f7426b15008ba22a2835dec88c9d
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-10-02 17:38:54 +0300

    Added lombok to TicketEntity and UserEntity

--- src/main/java/ua/edu/ukma/event_management_system/entity/TicketEntity.java
+++ src/main/java/ua/edu/ukma/event_management_system/entity/TicketEntity.java
@@ -1,11 +1,16 @@
 package ua.edu.ukma.event_management_system.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "ticket")
 public class TicketEntity {
     @Id
+    @Setter(AccessLevel.NONE)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
@@ -20,46 +25,9 @@ public class TicketEntity {
     @Column(nullable = false)
     private int price;
 
-    public TicketEntity() {}
-
-    // Constructor (optional)
     public TicketEntity(UserEntity user, EventEntity event, int price) {
         this.user = user;
         this.event = event;
-        this.price = price;
-    }
-
-    // Getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
-
-    public EventEntity getEvent() {
-        return event;
-    }
-
-    public void setEvent(EventEntity event) {
-        this.event = event;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
         this.price = price;
     }
 }
--- src/main/java/ua/edu/ukma/event_management_system/entity/UserEntity.java
+++ src/main/java/ua/edu/ukma/event_management_system/entity/UserEntity.java
@@ -4,10 +4,15 @@ import jakarta.persistence.*;
 import ua.edu.ukma.event_management_system.domain.UserRole;
 
 import java.util.List;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "users") // user is reserved keyword
 public class UserEntity {
+    @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -37,10 +42,6 @@ public class UserEntity {
     @ManyToMany(mappedBy = "users")
     private List<EventEntity> events;
 
-    // Default constructor (required by JPA)
-    public UserEntity() {}
-
-    // Constructor with parameters (optional)
     public UserEntity(UserRole userRole, String username, String firstName, String lastName, String email, String phoneNumber, int age) {
         this.userRole = userRole;
         this.username = username;
@@ -49,71 +50,5 @@ public class UserEntity {
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.age = age;
-    }
-
-    // Getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UserRole getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 }
