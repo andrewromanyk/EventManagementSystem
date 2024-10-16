69645bbc09ae73b0b76344c4626f88b542e0e58c
Andrii Romaniuk <andrewrom321@gmail.com>
2024-10-16 18:23:32 +0300

    added custom appender and layout

---
+++ src/main/java/ua/edu/ukma/event_management_system/logger/CustomAppender.java
@@ -0,0 +1,38 @@
+package ua.edu.ukma.event_management_system.logger;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Layout;
+import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
+import org.slf4j.*;
+import ua.edu.ukma.event_management_system.controller.BuildingController;
+
+@AllArgsConstructor
+public class CustomAppender extends AppenderBase<ILoggingEvent> {
+
+    private Layout<ILoggingEvent> layout;
+
+    @Override
+    protected void append(ILoggingEvent iLoggingEvent) {
+        System.out.println(layout.doLayout(iLoggingEvent));
+    }
+
+    public static void main(String[] args) {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        CustomLayout layout = new CustomLayout();
+
+        CustomAppender app = new CustomAppender(layout);
+        app.setContext(loggerContext);
+        app.start();
+
+        Logger logg = (Logger) LoggerFactory.getLogger(CustomAppender.class);
+        logg.addAppender(app);
+
+        logg.info("Hello World");
+    }
+
+}
