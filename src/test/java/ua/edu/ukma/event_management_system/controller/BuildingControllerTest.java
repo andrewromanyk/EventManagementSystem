e72e960e0136098adf8119966d7e987a0e314495
Illia Krachylo <kraciloi@gmail.com>
2024-10-29 23:37:26 +0200

    add test, create basic error page, add exception handler

--- src/test/java/ua/edu/ukma/event_management_system/controller/BuildingControllerTest.java
+++ src/test/java/ua/edu/ukma/event_management_system/controller/BuildingControllerTest.java
@@ -13,16 +13,20 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ua.edu.ukma.event_management_system.config.SecurityConfiguration;
 import ua.edu.ukma.event_management_system.domain.Building;
 import ua.edu.ukma.event_management_system.dto.BuildingDto;
+import ua.edu.ukma.event_management_system.exceptions.handler.ControllerExceptionHandler;
 import ua.edu.ukma.event_management_system.service.interfaces.BuildingService;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(excludeAutoConfiguration = SecurityConfiguration.class)
-@ContextConfiguration(classes = BuildingController.class)
+@ContextConfiguration(classes = {BuildingController.class, ControllerExceptionHandler.class})
 @AutoConfigureMockMvc(addFilters = false)
 public class BuildingControllerTest {
 
@@ -35,19 +39,25 @@ public class BuildingControllerTest {
 	@MockBean
 	private BuildingService buildingService;
 
-	@BeforeEach
-	void setup() {
+	@Test
+	void testGetBuilding() throws Exception {
 		Building building1 = building1();
 		when(buildingService.getBuildingById(1L)).thenReturn(building1);
 		when(modelMapper.map(building1, BuildingDto.class)).thenReturn(building1Dto());
-	}
-
-	@Test
-	void testGetBuilding() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/building/1"))
 				.andExpect(status().isOk())
 				.andExpect(model().attributeExists("buildings"))
 				.andExpect(model().attribute("buildings", List.of(building1Dto())));
+	}
+
+	@Test
+	void testGetBuildingWithNonExistingId() throws Exception {
+		when(buildingService.getBuildingById(any()))
+				.thenThrow(new NoSuchElementException("Building not found: 666"));
+		mockMvc.perform(MockMvcRequestBuilders.get("/building/666"))
+				.andExpect(status().isNotFound())
+				.andExpect(model().attributeExists("error"))
+				.andExpect(model().attribute("error", "Building not found: 666"));
 	}
 
 	private Building building1() {
