2819a9c3b0850cf78d27f66f68ecf22a4474f9b9
Andrii Romaniuk <andrewrom321@gmail.com>
2024-11-25 22:48:29 +0200

    changed main

--- src/main/java/ua/edu/ukma/event_management_system/views/FormController.java
+++ src/main/java/ua/edu/ukma/event_management_system/views/FormController.java
@@ -7,22 +7,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.ui.Model;
+import ua.edu.ukma.event_management_system.domain.Event;
+import ua.edu.ukma.event_management_system.service.interfaces.EventService;
 import ua.edu.ukma.event_management_system.service.interfaces.TicketService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
 public class FormController {
 
-    private TicketService ticketService;
+    private EventService eventService;
 
     @Autowired
-    public void setTicketService(TicketService service){
-        this.ticketService = service;
+    public void setTicketService(EventService service){
+        this.eventService = service;
     }
 
     @GetMapping("/main")
-    public String mainPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-        model.addAttribute("tickets", ticketService.getAllTicketsForUser(userDetails.getUsername()));
+    public String mainPage(Model model) throws IOException {
+        List<Event> events = eventService.getAllEvents();
+        Map<Integer, String> imageMap = new HashMap<>();
+        for (Event event : events) {
+            if (event.getImage() != null) {
+                String base64Image = "data:image/png;base64," + Base64.getEncoder().encodeToString(event.getImage());
+                imageMap.put(event.getId(), base64Image);
+            }
+            else {
+                String base64Image = "data:image/png;base64," + Base64.getEncoder().encodeToString(Files.readAllBytes(Path.of("src/main/resources/stock_photo.jpg")));
+                imageMap.put(event.getId(), base64Image);
+            }
+        }
+
+        model.addAttribute("events", events);
+        model.addAttribute("imageMap", imageMap);
         return "main";
     }
 }
