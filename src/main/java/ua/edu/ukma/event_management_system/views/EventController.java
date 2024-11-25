7fada14cc4aac8941ffd5039c8e5d86a8abe17d3
Anton <antonruban03012005@gmail.com>
2024-11-25 23:24:37 +0200

    fix todo

--- src/main/java/ua/edu/ukma/event_management_system/views/EventController.java
+++ src/main/java/ua/edu/ukma/event_management_system/views/EventController.java
@@ -92,11 +92,11 @@ public class EventController {
 
 	@GetMapping("/create")
 	public String showCreateEventForm(Model model) {
-		List<BuildingDto> buildings = buildingService.getAllBuildings()
-				.stream()
-				.map(this::toDto)
-				.toList();
-		model.addAttribute("buildings", buildings);
+//		List<BuildingDto> buildings = buildingService.getAllBuildings()
+//				.stream()
+//				.map(this::toDto)
+//				.toList();
+//		model.addAttribute("buildings", buildings);
 		model.addAttribute("event", new EventDto());
 		return "events/event-form";
 	}
