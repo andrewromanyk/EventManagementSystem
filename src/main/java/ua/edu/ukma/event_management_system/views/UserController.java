30a4b4f4db533e359301f77b1d3f6ed978a7ef85
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-11-26 20:44:50 +0200

    not working ticket frontend

---
+++ src/main/java/ua/edu/ukma/event_management_system/views/UserController.java
@@ -0,0 +1,160 @@
+package ua.edu.ukma.event_management_system.views;
+
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ua.edu.ukma.event_management_system.domain.User;
+import ua.edu.ukma.event_management_system.dto.UserDto;
+import ua.edu.ukma.event_management_system.service.interfaces.UserService;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("user")
+@ConditionalOnExpression("${api.user.enable}")
+public class UserController {
+    private ModelMapper modelMapper;
+    private UserService userService;
+
+    @Autowired
+    public void setModelMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public String getBuilding(@PathVariable long id, Model model) {
+        UserDto user = toDto(userService.getUserById(id));
+
+        model.addAttribute("user", user);
+//        model.addAttribute("ratings", ratings);
+        return "user/user-details";
+    }
+
+    @GetMapping("/")
+    public String getBuildings(Model model) {
+        List<UserDto> users;
+        users = userService.getAllUsers()
+                .stream()
+                .map(this::toDto)
+                .toList();
+
+
+//        Map<Integer, Double> buildingAvgRating = new HashMap<>();
+//        buildings.forEach(b -> {
+//            if (b.getRating() != null && !b.getRating().isEmpty()) {
+//                buildingAvgRating.put(
+//                        b.getId(),
+//                        b.getRating()
+//                                .stream()
+//                                .mapToInt(rId -> buildingService.getRatingById(rId).getRating())
+//                                .average()
+//                                .getAsDouble()
+//                );
+//            }
+//        });
+
+        model.addAttribute("buildings", users);
+//        model.addAttribute("avgRatings", buildingAvgRating);
+        return "buildings/building-list";
+    }
+
+//    @PostMapping("/")
+//    public String createNewBuilding(@ModelAttribute("buildingDto") @Valid UserDto buildingDto,
+//                                    BindingResult bindingResult, Model model) {
+//        if(bindingResult.hasErrors()){
+//            List<String> errors = new ArrayList<>();
+//            bindingResult.getFieldErrors().forEach(error -> {
+//                String errorMessage = "Error for " + error.getField() + " field: " + error.getDefaultMessage();
+//                errors.add(errorMessage);
+//            });
+//
+//            model.addAttribute("errors", errors);
+//            return "error";
+//        }
+//
+//        if(buildingDto.getDescription() == null || buildingDto.getDescription().isEmpty()){
+//            RestClient client = RestClient.create();
+//            String defaultDescription = client.get()
+//                    .uri("https://baconipsum.com/api/?type=meat-and-filler&sentences=2&format=text")
+//                    .retrieve()
+//                    .body(String.class);
+//            buildingDto.setDescription(defaultDescription);
+//        }
+//
+//        buildingService.createBuilding(buildingDto);
+//        return "redirect:/building/";
+//    }
+//
+//    @GetMapping("/create")
+//    public String createBuildingForm(Model model) {
+//        model.addAttribute("buildingDto", new BuildingDto());
+//        return "buildings/building-form";
+//    }
+//
+//    @GetMapping("/{id}/edit")
+//    public String editBuildingForm(@PathVariable long id, Model model){
+//        BuildingDto buildingDto = toDto(buildingService.getBuildingById(id));
+//        model.addAttribute("buildingDto", buildingDto);
+//        return "buildings/building-form";
+//    }
+//
+//    @PutMapping("/{id}")
+//    public String updateBuilding(@PathVariable long id, @Valid BuildingDto buildingDto,
+//                                 BindingResult bindingResult, Model model) {
+//        if(bindingResult.hasErrors()){
+//            List<String> errors = new ArrayList<>();
+//            bindingResult.getFieldErrors().forEach(error -> {
+//                String errorMessage = "Error for " + error.getField() + " field: " + error.getDefaultMessage();
+//                errors.add(errorMessage);
+//            });
+//            model.addAttribute("errors", errors);
+//            return "error";
+//        }
+//        buildingService.updateBuilding(id, buildingDto);
+//        return "redirect:/building/";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String deleteBuilding(@PathVariable long id, Model model) {
+//        try{
+//            buildingService.deleteBuilding(id);
+//        }catch(DataIntegrityViolationException ex){
+//            List<String> errors = new ArrayList<>();
+//            errors.add("Cannot delete building. It is referenced by other entities.");
+//            model.addAttribute("errors", errors);
+//            return "error";
+//        }
+//        return "redirect:/building/";
+//    }
+    //
+//    @PostMapping("/{id}/rate")
+//    public void rateBuilding(@PathVariable long id) {
+//    }
+//
+//    @GetMapping("/{buildingId}/{rating}")
+//    @ResponseBody
+//    public List<BuildingRatingDto> getRated(@PathVariable long buildingId, @PathVariable byte rating) {
+//        return buildingService.getAllByBuildingIdAndRating(buildingId, rating)
+//                .stream()
+//                .map(this::toDto)
+//                .toList();
+//    }
+//
+    private UserDto toDto(User user) {
+        return modelMapper.map(user, UserDto.class);
+    }
+
+//    private B toDto(BuildingRating buildingRating) {
+//        return modelMapper.map(buildingRating, BuildingRatingDto.class);
+//    }
+}
